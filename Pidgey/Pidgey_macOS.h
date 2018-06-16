1f65d7b69ca95f06b5c2a765395c327e75e36c42
Carlos Duclos <darkzeratul64@gmail.com>
2018-06-16 18:22:40 -0500

    Added info plist

---
+++ Pidgey/Pidgey_macOS.h
@@ -0,0 +1,18 @@
+//
+//  Pidgey_macOS.h
+//  Pidgey-macOS
+//
+//  Created by Carlos Duclos on 6/16/18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Pidgey_macOS.
+FOUNDATION_EXPORT double Pidgey_macOSVersionNumber;
+
+//! Project version string for Pidgey_macOS.
+FOUNDATION_EXPORT const unsigned char Pidgey_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pidgey_macOS/PublicHeader.h>
+
+
