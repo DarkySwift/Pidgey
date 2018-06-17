e65533b01918654f11e8dde598915f19c29dcfb1
Carlos Duclos <darkzeratul64@gmail.com>
2018-06-16 23:33:43 -0500

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -2,24 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "Pidgey",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Pidgey",
-            targets: ["Pidgey"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Pidgey",
-            dependencies: []),
-        .testTarget(
-            name: "PidgeyTests",
-            dependencies: ["Pidgey"]),
+        Target(
+            name: "Pidgey")
     ]
 )
